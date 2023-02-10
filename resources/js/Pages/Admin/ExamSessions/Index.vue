2e74d1b8ed53dd041710756bde3c77066e9982be
enzab <enciksabri90@gmail.com>
2023-02-10 20:21:55 +0700

    Menambahkan tombol + pada halaman index Exam Session

--- resources/js/Pages/Admin/ExamSessions/Index.vue
+++ resources/js/Pages/Admin/ExamSessions/Index.vue
@@ -58,8 +58,7 @@
                                         <td>{{ exam_session.start_time }}</td>
                                         <td>{{ exam_session.end_time }}</td>
                                         <td class="text-center">
-
-
+                                            <Link class="btn btn-sm btn-primary border-0 shadow me-2" type="button" :href="`/admin/exam_sessions/${exam_session.id}`"><i class="fa fa-plus-circle"></i></Link>
                                         </td>
                                     </tr>
                                 </tbody>
