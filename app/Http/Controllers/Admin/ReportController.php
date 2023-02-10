5c3771e705b00050cf248e468be138f408a725e2
enzab <enciksabri90@gmail.com>
2023-02-11 03:40:17 +0700

    Menambahkan Method Export di ReportController

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Carbon\Carbon;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Exports\GradesExport;
 use App\Models\Exam;
 use App\Models\Grade;
 use App\Models\ExamSession;
@@ -53,5 +56,24 @@ class ReportController extends Controller
             'exams' => $exams,
             'grades' => $grades
         ]);
+    }
+    
+    public function export(Request $request) {
+
+        //get exam
+        $exam = Exam::with('lesson', 'classroom')
+                ->where('id', $request->exam_id)
+                ->first();
+
+        //get exam session
+        $exam_session = ExamSession::where('exam_id', $exam->id)->first();
+
+        //get grades / nilai
+        $grades = Grade::with('student', 'exam.classroom', 'exam.lesson', 'exam_session')
+                ->where('exam_id', $exam->id)
+                ->where('exam_session_id', $exam_session->id)        
+                ->get();
+
+        return Excel::download(new GradesExport($grades), 'grade : '.$exam->title.' — '.$exam->lesson->title.' — '.Carbon::now().'.xlsx');
     }
 }
